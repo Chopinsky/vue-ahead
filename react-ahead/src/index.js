ee9969396e8dbe096e850ab208822292b4f0f705
ZUO, JACOB [ITUIS] <zuo@iastate.edu>
2020-03-19 15:05:40 -0500

    scolfolding the project

---
+++ react-ahead/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ReactAhead extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <div>
+          
+        </div>
+      </div>
+    )
+  }
+}
