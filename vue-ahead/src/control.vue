e40ee3f909c03230a10a95f6af8c5845e11655fb
ZUO, JACOB [ITUIS] <zuo@iastate.edu>
2020-04-07 10:22:51 -0500

    adding vue control and example site

---
+++ vue-ahead/src/control.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="control-wrapper">
+    <h1>control ... </h1>
+  </div>
+</template>
+
+<script>
+export default {
+	inheritAttrs: false,
+	name: "VueAhead",
+	data() {
+		console.log('vue ahead loaded');
+
+		return {
+
+		};
+	},
+};
+</script>
+
+<style scoped>
+.control-wrapper {
+  padding: 10px;
+}
+</style>
---
+++ vue-ahead/src/main.js
@@ -0,0 +1,1 @@
+export VueAhead from './control.vue';
