4c01c1876c40d186fb7b1714de4d7e48cecc7f2f
Jacob Zuo <chopinsky@live.com>
2020-06-03 22:38:27 -0500

    adding publish scripts

---
+++ vue-ahead/scripts/publish.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const pages = require('gh-pages');
+const path = require('path');
+
+const dest = path.resolve(__dirname, "../docs/.vuepress/dist");
+console.log('publishing doc from:', dest);
+
+pages.publish(dest, function (err) {
+  console.error(err);
+});
