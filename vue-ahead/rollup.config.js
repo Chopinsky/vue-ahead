f9c43dc9acaf1e6df808e4e1fe2e4b98a868483d
Jacob Zuo <zuo@iastate.edu>
2020-06-17 17:29:02 -0500

    update

--- vue-ahead/rollup.config.js
+++ vue-ahead/rollup.config.js
@@ -1,5 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import vue from 'rollup-plugin-vue';     // Handle .vue SFC files
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 
 export default {
